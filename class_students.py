0b5a1eeae4caeab2fbae2efaa601441020f72861
Remezov Dmitry <darknessdizi@gmail.com>
2022-09-11 01:35:35 +0800

    add metod rate_hw in class Student and __init__ class Lecture

--- class_students.py
+++ class_students.py
@@ -13,6 +13,22 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {} 
+
+    def rate_hw(self, lecture, course, grade):
+
+        '''Метод добавляет оценку лектору за предоставленный курс при условии,
+что лектор является объктом класса Lecture, предоставленный курс закреплен за
+указанным лектором и данный курс входит в список курсов текущего студента'''
+        
+        if (isinstance(lecture, Lecture) and
+            course in lecture.courses_attached and
+            course in self.courses_in_progress):
+            if course in lecture.grades:
+                lecture.grades[course] += [grade]
+            else:
+                lecture.grades[course] = [grade]
+        else:
+            return f'Error for {lecture.name} {lecture.surname}'
  
 class Mentor:
 
@@ -28,7 +44,16 @@ class Mentor:
         self.courses_attached = []
 
 class Lecture(Mentor):
-    pass
+
+    '''Создает учителей-лекторов'''
+
+    def __init__(self, name, surname):
+
+        '''Конструктор класса Lecture.
+        Добавляет лектора и определяет его атрибуты'''
+
+        super().__init__(name, surname)
+        self.grades = {}
 
 class Reviewer(Mentor):
 
@@ -50,15 +75,28 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка' 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+best_student = Student('Ruoy', 'Eman', 'man')
+best_student.courses_in_progress += ['Python', 'Java']
  
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
+
+cool_lecture = Lecture('Irina', 'Shayk')
+cool_lecture.courses_attached += ['Python', 'Java', 'C++']
+
+best_student.rate_hw(cool_lecture, 'Python', 9)
+best_student.rate_hw(cool_lecture, 'Python', 6)
+best_student.rate_hw(cool_lecture, 'Python', 9)
+print(best_student.rate_hw(cool_lecture, 'Java', 1))
+print(best_student.rate_hw(cool_lecture, 'C++', 5))
+
+print(best_student.rate_hw(cool_reviewer, 'Python', 3))
  
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
-
+print(best_student.__dict__)
+print(cool_lecture.__dict__)
+print(cool_reviewer.__dict__)
