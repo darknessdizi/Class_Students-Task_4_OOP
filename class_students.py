7bb0d064683142a8184c86b80586f87fd4899587
Remezov Dmitry <darknessdizi@gmail.com>
2022-09-11 00:23:36 +0800

    Initial project

---
+++ class_students.py
@@ -0,0 +1,63 @@
+class Student:
+
+    '''Создает студентов'''
+
+    def __init__(self, name, surname, gender):
+
+        '''Конструктор класса Student.
+        Добавляет студента и определяет его атрибуты'''
+
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {} 
+ 
+class Mentor:
+
+    '''Создает учителей'''
+
+    def __init__(self, name, surname):
+
+        '''Конструктор класса Mentor.
+        Добавляет учителя и определяет его атрибуты'''
+
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+
+        '''Метод добавляет оценку студенту за пройденный курс при условии,
+что студент является объктом класса Student, пройденный курс закреплен за
+текущим учителем и данный курс входит в список курсов указанного студента'''
+        
+        if (isinstance(student, Student) and
+            course in self.courses_attached and
+            course in student.courses_in_progress):
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+class Lecture(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    pass 
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_lector = Lecture('Some', 'Buddy')
+cool_lector.courses_attached += ['Python']
+ 
+cool_lector.rate_hw(best_student, 'Python', 10)
+cool_lector.rate_hw(best_student, 'Python', 10)
+cool_lector.rate_hw(best_student, 'Python', 10)
+ 
+print(best_student.grades)
+
